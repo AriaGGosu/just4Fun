903f4b8886fb5503ba6dc0c17b6dffcdb715e04e
Hector Vicencio Reyes <hector.vicencio.reyes@gmail.com>
2020-08-10 20:13:22 -0400

    Updates

---
+++ precache-manifest.32a1b7521f86cb702ee3e188859065ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3138d8ae1ec4369f720c291924d572",
+    "url": "/just4Fun/index.html"
+  },
+  {
+    "revision": "5f2a3919b1b53861cd5c",
+    "url": "/just4Fun/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "714359ac221eb521c890",
+    "url": "/just4Fun/static/js/2.7d72e063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/just4Fun/static/js/2.7d72e063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2a3919b1b53861cd5c",
+    "url": "/just4Fun/static/js/main.4e1c73bb.chunk.js"
+  },
+  {
+    "revision": "98584d29902675389d85",
+    "url": "/just4Fun/static/js/runtime-main.78213374.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/just4Fun/static/media/logo.5d5d9eef.svg"
+  }
+]);
